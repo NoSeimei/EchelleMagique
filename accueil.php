5e3b47b99b3a54d7670799d9e7fba8bb6459b0d4
DelahogueJulien <de-la-hogue.julien@orange.fr>
2021-09-23 16:22:20 +0400

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,3 +1,5 @@
+<!doctype html>
+<html lang="en">
 <head>
     <title>Accueil</title>
     <!-- Required meta tags -->
@@ -8,24 +10,39 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
+<body> 
 
+<?php
+include ("connexion.php");
 
-<body>
-<div class="row">
+try{ 
+    //on récupère tout les questionnaire
+    $requete = $db->prepare("SELECT * FROM `Questionnaire` ");
+    $requete->execute();
+    $requete->setFetchMode(PDO::FETCH_CLASS, 'Questionnaire');
+    $questionnaire = $requete->fetchAll();
+
+    foreach ($questionnaire as $questionnaires){
+?>
+        <div class="row">
     <div class="col-4 "></div>
         
 <div class=" col-4card  mt-sm-5 my-1" style="width: 18rem;">
   <div class="card-body  ml-sm-4 pl-sm-5 pt-2">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title"><?php $questionnaires->getTitre ?></h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 </div>
 <div class="col-4 "></div>
+ <?php   } ?>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
+ </html>
